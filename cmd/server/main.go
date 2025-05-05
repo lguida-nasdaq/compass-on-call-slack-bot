d587bee7d320f6ce64eeeb985a78b040e9bbc01d
Leandro Guida <leandro.guida@nasdaq.com>
2025-05-05 15:41:15 -0400

    Refactor main to use Kong

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,32 +1,70 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"log/slog"
 	"os"
-	"strconv"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/alecthomas/kong"
 	"github.com/metriodev/pompiers/internal/adapters/api"
 	"github.com/metriodev/pompiers/internal/app"
 	"github.com/metriodev/pompiers/internal/server"
 )
 
-func main() {
-	apiUser := os.Getenv("USER_EMAIL")
-	apiKey := os.Getenv("API_KEY")
-	cloudId := os.Getenv("ATLASSIAN_CLOUD_ID")
+type Cli struct {
+	Run     RunCMD `cmd:"" description:"Run the server"`
+	Timeout int    `default:"5000" help:"Timeout for shutdown in seconds"`
+}
 
-	compassClient := api.NewCompassClient(apiUser, apiKey, cloudId)
-	jiraClient := api.NewJiraClient(apiUser, apiKey)
+type RunCMD struct {
+	Port               int    `default:"8080" help:"Port to run the server on"`
+	Host               string `help:"Host to run the server on"`
+	AtlassianApiKey    string `required:"" help:"Atlassian API key"`
+	AtlassianApiUser   string `required:"" help:"Atlassian API user"`
+	AtlassianCloudId   string `required:"" help:"Atlassian cloud ID"`
+	SlackSigningSecret string `required:"" help:"Slack signing secret"`
+}
 
-	app := app.NewApp(compassClient, jiraClient)
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		log.Fatalf("Failed to get available port: %v", err)
-	}
-	host := os.Getenv("HOST")
+func (r RunCMD) Run(cli *Cli) error {
+	app := app.NewApp(
+		api.NewCompassClient(r.AtlassianApiUser, r.AtlassianApiKey, r.AtlassianCloudId),
+		api.NewJiraClient(r.AtlassianApiUser, r.AtlassianApiKey),
+	)
 
-	srv := server.NewServer(app, host, port, os.Getenv("SLACK_SIGNING_SECRET"))
+	srv := server.NewServer(app, r.Host, r.Port, r.SlackSigningSecret)
 	if err := srv.Start(); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+		slog.Error("Error starting server", slog.String("error", err.Error()))
+		return fmt.Errorf("Error starting server: %v", err)
 	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case s := <-signalChan:
+		slog.Info("Termination signal received, shutting down...", slog.String("signal", s.String()))
+	}
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Duration(cli.Timeout)*time.Millisecond)
+	defer shutdownCancel()
+
+	err := srv.Stop(shutdownCtx)
+	if err != nil {
+		return fmt.Errorf("s.Stop: %v", err)
+	}
+
+	slog.Info("Shut down complete")
+	return nil
+}
+
+func main() {
+	slog.Info("Starting CLI")
+	var cli Cli
+	kctx := kong.Parse(&cli, kong.DefaultEnvars(""))
+	err := kctx.Run()
+	kctx.FatalIfErrorf(err)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/metriodev/pompiers
 go 1.24.2
 
 require (
+	github.com/alecthomas/kong v1.10.0
 	github.com/slack-go/slack v0.16.0
 	golang.org/x/sync v0.13.0
 )
